ef4145c1e6db37130f692aeec44a2617df511346
Daniel Amas <amasdaniel3@gmail.com>
2024-11-11 13:02:11 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/notemeflutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.notemeflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:notemeflutter/screens/home_screen.dart';
+
+void main() {
+  runApp(const NoteMe());
+}
+
+class NoteMe extends StatelessWidget {
+  const NoteMe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NoteMe',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.red,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
